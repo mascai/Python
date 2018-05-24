dc61dcdc82f2eb6a172968156a3d32db431cc183
mascai <msumoskalenko@gmail.com>
2018-05-25 01:18:36 +0300

    Reques example

--- 0_coursera/0_id.py
+++ 0_coursera/0_id.py
@@ -1,1 +1,24 @@
-print("Test")
+import requests
+
+def get_location_info():
+	return requests.get("http://freegeoip.net/json/").json()
+
+if __name__ == "__main__":
+	data = get_location_info()
+	for key, item in data.items():
+		print (key, "---", item)
+
+
+'''
+metro_code --- 0
+region_name --- Moscow
+time_zone --- Europe/Moscow
+city --- Moscow
+zip_code --- 102630
+country_code --- RU
+country_name --- Russia
+longitude --- 37.2156
+latitude --- 55.052
+region_code --- MOW
+
+'''
