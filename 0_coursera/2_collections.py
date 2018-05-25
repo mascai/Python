d11aa4674e9ec7d716a08b9ffd8f9e91afed55b9
mascai <msumoskalenko@gmail.com>
2018-05-25 19:05:14 +0300

    Find the most common word

---
+++ 0_coursera/2_collections.py
@@ -0,0 +1,41 @@
+import operator
+from collections import Counter
+
+zen = """Beautiful is better than ugly.
+Explicit is better than implicit.
+Simple is better than complex.
+Complex is better than complicated.
+Flat is better than nested.
+Sparse is better than dense.
+Readability counts.
+Special cases aren't special enough to break the rules.
+Although practicality beats purity.
+Errors should never pass silently.
+Unless explicitly silenced.
+In the face of ambiguity, refuse the temptation to guess.
+There should be one-- and preferably only one --obvious way to do it.
+Although that way may not be obvious at first unless you're Dutch.
+Now is better than never.
+Although never is often better than *right* now.
+If the implementation is hard to explain, it's a bad idea.
+If the implementation is easy to explain, it may be a good idea.
+Namespaces are one honking great idea -- let's do more of those!
+"""
+zen_map = dict()
+cleaned_list = []
+for word in zen.split():
+    cleaned_word = word.strip('.,!-*').lower()
+    cleaned_list.append(cleaned_word) # for easy way
+    if cleaned_word not in zen_map:
+        zen_map[cleaned_word] = 0
+        
+    zen_map[cleaned_word] += 1
+    
+vec = zen_map.items()
+
+sorted_vec = sorted(vec, key = operator.itemgetter(1), reverse = True)
+
+print(sorted_vec[:3])
+
+# Easy way
+print(Counter(cleaned_list).most_common(3))
